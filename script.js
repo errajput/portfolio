9ca82f9cc1b804b4765872bbeac5fc94b17f07c8
disha rajput <dishacsevira@gmail.com>
2025-09-16 16:42:01 +0530

    trying to add email but comment it

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ document.addEventListener("DOMContentLoaded", () => {
     navLinks.classList.toggle("active");
   });
 });
+
+// initialize EmailJS
+// (function () {
+//   emailjs.init("dishacsevira@gmail.com");
+// })();
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const form = document.querySelector(".contact-form");
+
+//   if (form) {
+//     form.addEventListener("submit", function (e) {
+//       e.preventDefault();
+
+//       emailjs
+//         .send("dishacsevira@gmail.com", "dishacsevira@gmail.com", {
+//           from_name: document.getElementById("name").value,
+//           from_email: document.getElementById("email").value,
+//           message: document.getElementById("message").value,
+//         })
+//         .then(
+//           () => {
+//             alert("Message sent successfully!");
+//             form.reset();
+//           },
+//           (error) => {
+//             alert("Failed to send message. Please try again.");
+//             console.error(error);
+//           }
+//         );
+//     });
+//   }
+// });
